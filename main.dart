3fc134649a528e1a089d6a83d98a199dc9e187dd
Abhiram-ks <abhiramks0001@gmial.com>
2026-02-27 18:29:44 +0530

    initial commit

---
+++ main.dart
@@ -0,0 +1,9 @@
+//**
+//1. Encapsulation
+//2. Inheritance
+//3. Polymorphism
+//4. Abstraction */
+
+//!3. Polymorphism: It is the ability of an object to take an many forms. It allows us to use a single interface to represent diffrent types of objects. There are two types of polymorphism: compile-time polymorphism and run-time polymorphism. 
+//Compile-time polymorphism: It is achieved through method overloading and operator overloading. Method overloading is the ability to define multiple methods with the smame name but different parameters. Operator overloading is the ability to define custom behavior for operators when they are used with user-defined types. 
+//Run-time polymorphism: It is achieved through method overriding. Method overriding is the ability of a subclass to provide a specific implementation of a method that is already defined in its superclass.
